af0b868f61142114013e50fc88893c13cf7bef8b
Lauri Tirkkonen <lotheac@iki.fi>
2017-12-27 22:04:19 +0200

    add missing sin_bar.h

---
+++ src/sin_bar.h
@@ -0,0 +1,10 @@
+#ifndef SIN_BAR_H
+#define SIN_BAR_H
+#define SIN_COUNT 128
+int sin_bar(void);
+const u8 *sin_bar_data;
+u8 sin_bar_1_index;
+u8 sin_bar_2_index;
+u8 sin_bar_draw_line_y;
+u8 sin_bar_draw_line_y2;
+#endif
