f4bd91b133c0aa942177d0b7f7b66b419ca55ec9
Lauri Tirkkonen <lotheac@iki.fi>
2017-12-27 21:30:07 +0200

    main: scroll text horizontally too

--- src/main.c
+++ src/main.c
@@ -1,14 +1,17 @@
 #include <genesis.h>
 #include "sin_bar.h"
 
+#define MESSAGE "Oispa kaljaa"
+
 int main() {
-  u8 ypos;
+  u8 ypos, xpos;
   u8 i = 17;
   VDP_setTextPalette(1);
   while (1) {
     sin_bar();
-    ypos = sin_bar_data[sin_bar_1_index];
-    VDP_drawText("Oispa kaljaa", 2, ypos+1);
+    xpos = sin_bar_data[(sin_bar_1_index+9)%128] + sizeof(MESSAGE)/2;
+    ypos = sin_bar_data[sin_bar_1_index] + 1;
+    VDP_drawText(MESSAGE, xpos, ypos);
     VDP_waitVSync();
   }
   return (0);
