066fe7071b114d15593f36489fddcbd149eeadb6
Jere Nevalainen <jere.nevalainen@gmail.com>
2018-03-28 21:17:24 +0300

    Wavy lake mirror effect with static waves

--- src/interrupt_test.c
+++ src/interrupt_test.c
@@ -1,22 +1,22 @@
 #include <genesis.h>
 #include <vdp.h>
 #include "interrupt_test.h"
-#include "sprite.h"
 #include "gfx.h"
 
 int current_scroll = 0;
 int scanline_number = 0;
 
 void h_interrupt_cb(void){
-    if(scanline_number > 60){
-        current_scroll--;
+    if(scanline_number > 70){
+        current_scroll -= 4;
+        if(scanline_number%2 == 0){
+            VDP_setHorizontalScroll(PLAN_WINDOW, 2);
+        } else {
+            VDP_setHorizontalScroll(PLAN_WINDOW, -2);        
+        }
     }
-    /*if(scanline_number%2 == 0){
-        VDP_setHorizontalScrollLine(PLAN_WINDOW, scanline_number, 5, 1, TRUE);
-    }*/
-    scanline_number++;
     VDP_setVerticalScroll(PLAN_WINDOW, current_scroll);
-    
+    scanline_number++;
 }
 
 void v_interrupt_cb(void){
@@ -27,13 +27,12 @@ void v_interrupt_cb(void){
 
 void init_interrupt_test(void)
 {
-	// disable interrupt when accessing VDP
 	SYS_disableInts();
 
 	VDP_setScreenHeight240();
 	VDP_setScreenWidth320();
 
-    VDP_setScrollingMode(HSCROLL_LINE, VSCROLL_PLANE);
+    VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);
     SYS_setHIntCallback(h_interrupt_cb);
     SYS_setVIntCallback(v_interrupt_cb);
     VDP_setHInterrupt(TRUE);
@@ -48,7 +47,6 @@ void init_interrupt_test(void)
 
 int interrupt_test(void)
 {
-	//SPR_update();
 	VDP_waitVSync();
 	return (0);
 }
@@ -57,6 +55,7 @@ int end_interrupt_test(void)
 {
     SYS_disableInts();
     SPR_end();
+    VDP_setHInterrupt(FALSE);
     SYS_enableInts();
     return (0);
 }
