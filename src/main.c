e908952fa56502790a9f34a5c29d03861ccc3062
Lauri Tirkkonen <lotheac@iki.fi>
2018-03-30 18:13:25 +0300

    integrate wave1

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "sin_bar.h"
 #include "cryptopts.h"
 #include "scroll.h"
+#include "wave1.h"
 
 int main(void)
 {
@@ -12,15 +13,18 @@ int main(void)
 	PSG_init();
 	SND_startPlay_VGM(sonic1);
 	sin_bar_init();
-	while (getTick() < 300*10)
+	while (getTick() < 300*1)
 		sin_bar();
+	wave1_init();
+	while (getTick() < 300*20)
+		wave1();
 	scroll_init();
 	/* TODO: scroll() maybe writes out of bounds / Gekko; if it's before
 	 * sin_bar(), sin_bar() bugs */
-	while (getTick() < 300*20)
+	while (getTick() < 300*30)
 		scroll();
 	/* FIXME crypto_pts never exits */
-	while (getTick() < 300*30)
+	while (getTick() < 300*90)
 		crypto_pts();
 	return 0;
 }
